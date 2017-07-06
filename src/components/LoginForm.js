e588f603fd15d132f39868011988724bfa6f75c0
Srivatsa S <srivatsashankaran@gmail.com>
2017-07-06 20:18:08 +0530

    adding state and setting it to a text : '' and then in the textinput used that as props for value and onChangeText

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,11 +4,17 @@ import { TextInput } from 'react-native';
 import { Card, Button, CardSection } from './common';
 
 class LoginForm extends React.Component{
+  state = { text: ''};
+
   render() {
     return(
       <Card>
         <CardSection>
-          <TextInput style={{ height: 20, width: 100}} />
+          <TextInput
+            value={this.state.text}
+            onChangeText={text => this.setState({ text })}
+            style={{ height: 20, width: 100}}
+          />
         </CardSection>
 
         <CardSection></CardSection>
