5b1fbda7819e128850411ea0b7591e5175f19dd8
Srivatsa S <srivatsashankaran@gmail.com>
2017-07-06 19:55:38 +0530

    adding the firebase client setup in the lifecycle method componentWillMount

--- App.js
+++ App.js
@@ -1,23 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Header } from './components/common';
+import firebase from 'firebase';
 
 export default class App extends React.Component {
+  componentWillMount() {
+    firebase.initializeApp({
+    apiKey: "AIzaSyAGde8BY3k3JIg2ktes7OvmZuKOFXI0R1Y",
+    authDomain: "authentication-for-reactnative.firebaseapp.com",
+    databaseURL: "https://authentication-for-reactnative.firebaseio.com",
+    projectId: "authentication-for-reactnative",
+    storageBucket: "",
+    messagingSenderId: "311397383975"
+  })
+  }
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+    return(
+      <View style={{ flex: 1 }}>
+        <Header headerText="Authentication" />
+        <AlbumList />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center'
+//   },
+// });
+
+// //App.js- Place code here for android
+//
+// //import a library to help create a Component
+// import React frpm 'react';
+// import { Text, AppRegistry } from 'react-native';
+//
+// //Create a Component
+// const App = () => {
+//   return(
+//     <Text>Some Text</Text>
+//   );
+// };
+//
+// //Render it to a device
+// AppRegistry.registerComponent('albums', () => App);
